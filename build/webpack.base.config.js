22ddc84224711cfef9369cdd3462661ea366479d
tana-gh <yozoranotakaku@gmail.com>
2020-03-28 16:27:08 +0900

    Fix public path

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -45,7 +45,7 @@ module.exports = Object.entries(TARGETS).map(([ key, target ]) => ({
     output: {
         filename: '[name].bundle.js',
         path: PATHS.dist,
-        publicPath: ''
+        publicPath: '/'
     },
     ...(!target.includes('web') ? {
         node: {
