fc592875c450ce7bcdb3fe4db13a7596935b5c5f
tana-gh <yozoranotakaku@gmail.com>
2020-04-15 20:11:55 +0900

    Fix production publicPath

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -49,7 +49,7 @@ module.exports = mode => Object.entries(TARGETS).map(([ key, target ]) => ({
     output: {
         filename: '[name].bundle.js',
         path: PATHS.dist,
-        publicPath: (process.env.IS_ELECTRON ? '' : '/')
+        publicPath: (process.env.IS_ELECTRON || mode == 'production' ? '' : '/')
     },
     ...(!target.includes('web') ? {
         node: {
