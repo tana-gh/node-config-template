cd5b53c2bc93797e5dae2f7a31b04927e1f5af34
tana-gh <yozoranotakaku@gmail.com>
2020-07-01 16:13:13 +0900

    Add tsconfig.react.json

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,22 @@
 
 module.exports = {
-    'moduleFileExtensions': [ 'js', 'jsx', 'ts', 'tsx' ],
-    'moduleDirectories': [ 'node_modules' ],
-    'moduleNameMapper': {
+    moduleFileExtensions: [ 'js', 'jsx', 'ts', 'tsx' ],
+    moduleDirectories: [ 'node_modules' ],
+    moduleNameMapper: {
         '\\.(css|s[ac]ss)$': 'identity-obj-proxy',
         '\\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)$': '<rootDir>/mocks/fileMock.js',
         '^@/(.+)': '<rootDir>/src/$1'
     },
-    'transform': {
+    transform: {
         '\\.(jsx?)$': 'babel-jest',
         '\\.(tsx?)$': 'ts-jest'
     },
-    'testMatch': [
+    testMatch: [
         '**/__tests__/**/*.([jt]s?(x))',
         '**/*.(spec|test).([jt]s?(x))'
     ],
-    'collectCoverage': true,
-    'collectCoverageFrom': [
+    collectCoverage: true,
+    collectCoverageFrom: [
         'src/**/*.([jt]s?(x))',
         '!**/node_modules/**'
     ]
