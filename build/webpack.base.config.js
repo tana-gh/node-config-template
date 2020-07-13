ec14310877d9bfdef6d01a431ab27c71016b63da
tana-gh <yozoranotakaku@gmail.com>
2020-07-14 08:37:02 +0900

    Fix copy-webpack-plugin settings

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -245,9 +245,9 @@ module.exports = mode => Object.entries(TARGETS).map(([ key, target ]) => ({
         
         new CopyWebpackPlugin({
             patterns: [
-                { from: `${PATHS.assets}/favicon/**/*`, to: 'assets/favicon/' },
-                { from: `${PATHS.assets}/images/**/*` , to: 'assets/images/'  },
-                { from: `${PATHS.assets}/misc/**/*`   , to: 'assets/misc/'    }
+                { from: `${PATHS.assets}/favicon/`, to: 'assets/favicon/' },
+                { from: `${PATHS.assets}/images/` , to: 'assets/images/'  },
+                { from: `${PATHS.assets}/misc/`   , to: 'assets/misc/'    }
             ]
         }),
 
